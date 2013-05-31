942289650bafcf775477b39e1dbf0af519def715
Jonathan Dean <jon@jonathandean.com>
2013-05-30 23:35:54 -0400

    basic code for first set of payments service slides

---
+++ magma-payments-service/app/controllers/customers_controller.rb
@@ -0,0 +1,17 @@
+class CustomersController < ApplicationController
+  def create
+    # Need to send a hash of :id, :first_name, :last_name, :email
+    result = Braintree::Customer.create({
+      :id => params[:id],
+      :first_name => params[:last_name],
+      :last_name => params[:last_name],
+      :email => params[:email]
+    })
+    # Build a hash we can send as JSON in the response
+    resp = { success: result.success?, message: (result.message rescue '') }
+    # Render JSON as the response
+    respond_to do |format|
+      format.json { render json: resp }
+    end
+  end
+end
